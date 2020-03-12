abb79c518cb1f4bd7fbfef179a480ade01872c74
AparnaMani357 <aparna.mani92@gmail.com>
2020-03-12 16:57:36 +0530

    Creating a conflict

--- src/test/java/UdemyAcademy/NewClas.java
+++ src/test/java/UdemyAcademy/NewClas.java
@@ -7,6 +7,6 @@ public class NewClas {
 	{
 
 	System.out.println("This is a new class added by NewFramework team-Aparna");
-	System.out.println("I checked out to your branch and modified something-Pankaj");
+	//System.out.println("I checked out to your branch and modified something-Pankaj");
 }
 }
