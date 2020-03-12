5e665fdacb9b14ce27a13ed7bef217585a674a09
AparnaMani357 <aparna.mani92@gmail.com>
2020-03-12 17:08:05 +0530

    resolved conflict

--- src/test/java/UdemyAcademy/NewClas.java
+++ src/test/java/UdemyAcademy/NewClas.java
@@ -8,6 +8,6 @@ public class NewClas {
 
 	System.out.println("This is a new class added by NewFramework team-Aparna");
 	//System.out.println("I checked out to your branch and modified something-Pankaj");
-	System.out.println("Hi hello");
+	System.out.println("Hi hello"+"Namaste");
 }
 }
